dc469eaf6df399dca19d0a78dcd9eb61b2b72676
Alisson-Henrique <torgatesousa@gmail.com>
2021-06-28 21:47:32 -0300

    User creation first implementation

---
+++ src/Manager.API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Manager.API.ViewModels;
+
+namespace Manager.API.Controllers{
+
+
+    [ApiController]
+    public class UserController : ControllerBase{
+
+        [HttpPost]
+        [Route("/api/v1/users/create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserViewModel userViewModel){
+
+            try
+            {
+                return Ok();
+            }
+            catch (Exception)
+            {
+                
+                return StatusCode(500, "Erro");
+            }
+
+        }
+
+    }
+}
