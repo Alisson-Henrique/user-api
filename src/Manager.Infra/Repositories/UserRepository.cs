c743b6e6423e70e2c7edc90c3cfbf9f33d9197c2
Alisson-Henrique <torgatesousa@gmail.com>
2021-07-01 23:41:27 -0300

    Implementation of user repository

---
+++ src/Manager.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manager.Domain.Entities;
+using Manager.Infra.Context;
+using Manager.Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Infra.Repositories{
+    public class UserRepository : BaseRepository<User>, IUserRepository{
+        private readonly ManagerContext _context;
+
+        public UserRepository(ManagerContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            var user = await _context.Users.Where(x =>x.Email.ToLower() == email.ToLower())
+                                    .AsNoTracking()
+                                    .ToListAsync();
+
+            return user.FirstOrDefault();
+        }
+
+        public async Task<User> GetByUsername(string username)
+        {
+            var user = await _context.Users.Where(x =>x.Username.ToLower() == username.ToLower())
+                                    .AsNoTracking()
+                                    .ToListAsync();
+
+            return user.FirstOrDefault();
+        }
+
+        public async Task<List<User>> SearchByUsername(string name)
+        {
+            var allUsers = await _context.Users.Where(x =>x.Name.ToLower().Contains(name.ToLower()))
+                                                .AsNoTracking()
+                                                .ToListAsync();
+
+            return allUsers;
+        }
+    }
+}
